d4265ef4f51468b12bbf7f024ed75b5836a2288a
daihe110 <daihe110@users.noreply.github.com>
2026-08-29 14:49:57 +0800

    PWA: manifest + service worker + icons

---
+++ sw.js
@@ -0,0 +1,42 @@
+const VERSION = "aipet-v1";
+const CORE = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(VERSION).then((c) => c.addAll(CORE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== VERSION).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  if (req.method !== "GET") return;
+  e.respondWith(
+    caches.match(req, { ignoreSearch: true }).then((hit) => {
+      if (hit) return hit;
+      return fetch(req)
+        .then((res) => {
+          const copy = res.clone();
+          if (res.ok && new URL(req.url).origin === location.origin) {
+            caches.open(VERSION).then((c) => c.put(req, copy));
+          }
+          return res;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
